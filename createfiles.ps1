dca210f97f0eb46f190dc8514df54b7d485418e2
Emanuel Palm <emanuel+github@pipe.how>
2024-05-10 12:02:07 +0200

    more notes

--- createfiles.ps1
+++ createfiles.ps1
@@ -1,8 +1,8 @@
 $DocSHA = git ls-tree HEAD ./docs/Contoso/MyDoc1.md --object-only
 git tag contoso/mydoc1/v1.0.1 $DocSHA
 
-git tag contoso/MyDoc1/v1.0.1 $(git hash-object -t blob -w ./docs/Contoso/MyDoc1.md)
+git tag -a contoso/MyDoc1/v1.0.2 $(git hash-object -t blob ./docs/Contoso/MyDoc1.md) -m "author:Emanuel, message:Updated MyDoc1.md"
 
-git tag -a contoso/MyDoc1/v1.0.1 $(git hash-object -t blob -w ./docs/Contoso/MyDoc1.md) -m "author:Emanuel"
+git show contoso/MyDoc1/v1.0.2
 
-git show contoso/MyDoc1/v1.0.1
+git tag -l --format='%(contents)' contoso/MyDoc1/v1.0.2 | Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
