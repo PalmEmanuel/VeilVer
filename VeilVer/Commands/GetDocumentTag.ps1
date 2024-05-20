fc4f7895ac9bf185d3f85206a94b6119d338cc42
Emanuel Palm <emanuel+github@pipe.how>
2024-05-20 20:10:30 +0200

    Integrate git tag commands into VeilVer module

---
+++ VeilVer/Commands/GetDocumentTag.ps1
@@ -0,0 +1,29 @@
+function Get-DocumentTag {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$DocumentPath
+    )
+
+    # Ensure git is available
+    if (-not (Get-Command "git" -ErrorAction SilentlyContinue)) {
+        Write-Error "Git is not available. Please install Git to use this command."
+        return
+    }
+
+    # Get all names that the file has had by looking at commits that have changed the file
+    # Automatically sorted by most recent (current) name first
+    $FileNames = git log --format="" --name-only --follow -- $DocumentPath | Select-Object -Unique
+
+    # Get all tags based on file names
+    foreach ($File in $FileNames) {
+        $TagName = $File -replace '.md', '' -replace 'docs/',''
+        $Tags = git tag -l --format='%(refname:short)' --sort=-creatordate "$TagName*"
+        foreach ($Tag in $Tags) {
+            $TagMessage = git tag -l --format='%(contents)' $Tag | Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
+            Write-Host "$Tag: $TagMessage"
+        }
+    }
+}
+
+Export-ModuleMember -Function Get-DocumentTag
---
+++ VeilVer/Demo/docs/Contoso/MyDoc3.md
@@ -0,0 +1,3 @@
+# Demo Document for VeilVer Module
+
+This document serves as another demo for testing the VeilVer module commands related to setting and getting git tags as metadata for documents within the repository.
