02f0b04f00be71fc347fbdb296d025dd519521b4
Emanuel Palm <emanuel+github@pipe.how>
2024-06-29 20:53:51 +0200

    Add Rename-VVVersion command (#20)
    * Add Sync-VVVersion command to update all historic tags / versions for a file that has changed name / path
    * Add unit tests for Sync-VVVersion command
    
    Adds integration tests for the `Sync-VVVersion` command and updates documentation to reflect its functionality.
    
    - **Integration Tests**: Adds a new test case in `tests/Integration.Tests.ps1` to verify that git tags are correctly updated after a file rename using `Sync-VVVersion`. This ensures the command's functionality is properly integrated with the rest of the system.
    - **Documentation**: Creates a new help document `docs/help/Sync-VVVersion.md` detailing the command's purpose, syntax, parameters, and usage examples. This provides users with the necessary information to utilize the command effectively.
    - **Command Implementation**: Implements the `Sync-VVVersion` PowerShell function in `source/Public/Sync-VVVersion.ps1`. This function checks for file existence, retrieves historical file names, updates git tags, and handles errors appropriately, ensuring a robust solution for renaming version tags.
    
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/PalmEmanuel/VeilVer/pull/20?shareId=1a62ba0a-adfb-4e5f-9c23-a2c946f15c44).
    
    * wip
    
    * renamed command to Sync-VVVersion
    
    * integration tests with git config
    
    * moved git config
    
    * moved git config to individual methods
    
    * git config in all places
    
    * psd1
    
    * moved git config

--- source/VeilVer.psd1
+++ source/VeilVer.psd1
@@ -66,15 +66,20 @@
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport = @(
         'Get-VVVersion'
-        'Set-VVVersion'
+        'Import-VVVersion'
+        'Push-VVVersion'
         'Remove-VVVersion'
+        'Set-VVVersion'
+        'Sync-VVVersion'
     )
     
     # Variables to export from this module
     VariablesToExport = @()
     
     # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
-    AliasesToExport = @()
+    AliasesToExport = @(
+        'Pull-VVVersion'
+    )
     
     # DSC resources to export from this module
     # DscResourcesToExport = @()
--- tests/Integration.Tests.ps1
+++ tests/Integration.Tests.ps1
@@ -1,78 +1,151 @@
-Describe "Integration Tests" {
-    BeforeAll {
-        # Need to set user config for the commits to work on all platforms in pipeline
-        git config --global user.name "VeilVer"
-        git config --global user.email "veilver@pipe.how"
-        # Set default branch name
-        git config --global init.defaultBranch main
+BeforeAll {
+    function NewGitRepo {
+        [CmdletBinding()]
+        param(
+            [Parameter(Mandatory)]
+            [ValidatePattern('\.git$', ErrorMessage = 'Name must end with ".git".')]
+            [string]$Path,
 
-        # Set up a temporary git repositories before tests
-        # Repo2 is the remote "origin"
-        # Repo1 is the clone
-        $Repo1 = "$TestDrive/repo1.git"
-        $Repo2 = "$TestDrive/repo2.git"
-        New-Item -ItemType Directory -Path $Repo1 -Force
-        New-Item -ItemType Directory -Path $Repo2 -Force
-        git init --bare $Repo2 --quiet
-        $null = git clone $Repo2 $Repo1 2>&1
-        Set-Location $Repo1
+            [Parameter()]
+            [switch]$Bare
+        )
 
-        # Create a new file in the temporary repository
-        $FileName = "testfile.txt"
-        $FilePath = Join-Path -Path $Repo1 -ChildPath $FileName
-        Set-Content -Path $FilePath -Value "Test content"
-        git add .
-        git commit -m "Initial commit with test file"
+        New-Item -ItemType Directory -Path $Path -Force
+    
+        if ($Bare.IsPresent) {
+            git init --bare $Path --quiet
+        }
+        else {
+            git init $Path --quiet
+        }
+
+        Push-Location $Path
+        SetGitConfig
+        Pop-Location
+    }
+    function SetGitConfig {
+        [CmdletBinding()]
+        param()
+
+        git config init.defaultBranch main
+        git config user.name 'VeilVer'
+        git config user.email 'veilver@pipe.how'
+    }
+    function CloneGitRepo {
+        [CmdletBinding()]
+        param(
+            [Parameter(Mandatory)]
+            [ValidatePattern('\.git$', ErrorMessage = 'Name must end with ".git".')]
+            [string]$Path,
+
+            [Parameter()]
+            [string]$DestinationPath
+        )
+    
+        New-Item -ItemType Directory -Path $DestinationPath -Force
+
+        $null = git clone $Path $DestinationPath 2>&1
+
+        Push-Location $DestinationPath
+        SetGitConfig
+        SeedGitRepoFiles
+        Pop-Location
+    }
+
+    function SeedGitRepoFiles {
+        [CmdletBinding()]
+        param()
+    
+        1..5 | ForEach-Object {
+            $TempFileName = "File$_.txt"
+            New-Item -Name $TempFileName -Value "Number $_"
+            git add $TempFileName
+            git commit -m "File $_ added"
+            1..5 | ForEach-Object {
+                Add-Content -Path "./$TempFileName" -Value ", then $_"
+                git add $TempFileName
+                git commit -m "Number $_ added to $TempFileName"
+            }
+        }
+    
         git push --quiet
+    }
+}
+
+Describe 'Integration Tests' {
+    
+    BeforeAll {
+        # Set up a temporary git repositories before tests
+        $LocalRepoPath = "$TestDrive/local.git"
+        $OriginRepoPath = "$TestDrive/origin.git"
+
+        Set-Location $TestDrive
+        
+        # Safety in case the code is run outside of Pester
+        if (-not [string]::IsNullOrWhiteSpace($TestDrive)) {
+            Remove-Item -Path $OriginRepoPath -Recurse -Force -ErrorAction SilentlyContinue
+            Remove-Item -Path $LocalRepoPath -Recurse -Force -ErrorAction SilentlyContinue
+        }
+        
+        NewGitRepo -Path $OriginRepoPath -Bare
+        CloneGitRepo -Path $OriginRepoPath $LocalRepoPath
         
         $SpecialCharacters = '!"#€%&/()=?`_:;*^©@£$∞§|[]≈±´\{}¡¥≠}¢¿@•Ωé®†µüıœπ˙æøﬁªß∂ƒ√ª˛ƒ∂ﬁßåäöÅÄÖ'
         $MetadataHash = @{
-            "Author" = "Emanuel Palm"
-            "Description" = "Testing the commands"
-            "Feature" = "N/A"
-            "ReviewedBy" = "None"
-            "ApprovedBy" = "Approver1"
-            "Tested" = $true
-            "Passed" = $true
-            "Category" = "IntegrationTest"
-            "Priority" = "High"
-            "ReleaseDate" = "2024-05-21"
-            "SpecialCharacters" = $SpecialCharacters
+            'Author'            = 'Emanuel Palm'
+            'Description'       = 'Testing the commands'
+            'Feature'           = 'N/A'
+            'ReviewedBy'        = 'None'
+            'ApprovedBy'        = 'Approver1'
+            'Tested'            = $true
+            'Passed'            = $true
+            'Category'          = 'IntegrationTest'
+            'Priority'          = 'High'
+            'ReleaseDate'       = '2024-05-21'
+            'SpecialCharacters' = $SpecialCharacters
         }
     }
+    
+    BeforeEach {
+        Set-Location $LocalRepoPath
 
-    It "Creates versions using Set-VVVersion" {
-        { Set-VVVersion -Path $FilePath -Version "1.0.0" -Metadata $MetadataHash -WarningAction SilentlyContinue } | Should -Not -Throw
-        { Set-VVVersion -Path $FilePath -Version "1.2.3" -Metadata $MetadataHash -WarningAction SilentlyContinue } | Should -Not -Throw
-        { Set-VVVersion -Path $FilePath -Version "1.10.1" -Metadata $MetadataHash -WarningAction SilentlyContinue } | Should -Not -Throw
-        { Set-VVVersion -Path $FilePath -Version "1.1.1" -Metadata $MetadataHash -WarningAction SilentlyContinue } | Should -Not -Throw
+        $File = (Get-ChildItem '*.txt')[0]
+        $FilePath = $File.FullName
+        $FileName = $File.Name
+    }
+    
+    It 'Creates versions using Set-VVVersion' {
+        { Set-VVVersion -Path $FilePath -Version '1.0.0' -Metadata $MetadataHash -WarningAction SilentlyContinue } | Should -Not -Throw
+        { Set-VVVersion -Path $FilePath -Version '1.2.3' -Metadata $MetadataHash -WarningAction SilentlyContinue } | Should -Not -Throw
+        { Set-VVVersion -Path $FilePath -Version '1.10.1' -Metadata $MetadataHash -WarningAction SilentlyContinue } | Should -Not -Throw
+        { Set-VVVersion -Path $FilePath -Version '1.1.1' -Metadata $MetadataHash -WarningAction SilentlyContinue } | Should -Not -Throw
     }
 
-    It "Retrieves versions in the right order using Get-VVVersion" {
+    It 'Retrieves versions in the right order using Get-VVVersion' {
         $Versions = Get-VVVersion -Path $FilePath
         $Versions.Count | Should -Be 4
         # Versions should be in descending version order, regardless of creation order
-        $Versions[0].Version.ToString() | Should -Be "1.10.1"
-        $Versions[1].Version.ToString() | Should -Be "1.2.3"
-        $Versions[2].Version.ToString() | Should -Be "1.1.1"
-        $Versions[3].Version.ToString() | Should -Be "1.0.0"
+        $Versions[0].Version.ToString() | Should -Be '1.10.1'
+        $Versions[1].Version.ToString() | Should -Be '1.2.3'
+        $Versions[2].Version.ToString() | Should -Be '1.1.1'
+        $Versions[3].Version.ToString() | Should -Be '1.0.0'
     }
 
-    It "Retrieves versions with correct information using Get-VVVersion" {
+    It 'Retrieves versions with correct information using Get-VVVersion' {
         $Versions = Get-VVVersion -Path $FilePath
         $Versions[0].Metadata.SpecialCharacters | Should -Be $SpecialCharacters
         $Versions[0].Metadata.Commit | Should -Be (git rev-parse --verify HEAD)
     }
 
-    It "Removes versions using Remove-VVVersion using Path and Version" {
+    It 'Removes versions using Remove-VVVersion using Path and Version' {
         $Versions = Get-VVVersion -Path $FilePath
         $Versions.Count | Should -Be 4
-        Remove-VVVersion -Path $FilePath -Version "1.0.0"
+        Remove-VVVersion -Path $FilePath -Version '1.0.0'
         $Versions = Get-VVVersion -Path $FilePath
         $Versions.Count | Should -Be 3
     }
 
-    It "Removes versions using Remove-VVVersion using Tag" {
+    It 'Removes versions using Remove-VVVersion using Tag' {
         $Versions = Get-VVVersion -Path $FilePath
         $Versions.Count | Should -Be 3
         Remove-VVVersion -Tag $Versions[1].Tag
@@ -80,26 +153,26 @@ Describe "Integration Tests" {
         $Versions.Count | Should -Be 2
     }
 
-    It "Checks out a file version with a commit using the -Checkout parameter" {
+    It 'Checks out a file version with a commit using the -Checkout parameter' {
         # Create a new version with specific content
-        $Version2Content = "This is a specific version 2 content."
-        $Version3Content = "This is a specific version 3 content."
+        $Version2Content = 'This is a specific version 2 content.'
+        $Version3Content = 'This is a specific version 3 content.'
 
         Set-Content -Path $FilePath -Value $Version2Content
         git add .
-        git commit -m "Committing version 2"
-        Set-VVVersion -Path $FilePath -Version "2.0.0" -Metadata @{ Description = "Specific version 2 for checkout test" }
+        git commit -m 'Committing version 2'
+        Set-VVVersion -Path $FilePath -Version '2.0.0' -Metadata @{ Description = 'Specific version 2 for checkout test' }
         
         Set-Content -Path $FilePath -Value $Version3Content
         git add .
-        git commit -m "Committing version 3"
-        Set-VVVersion -Path $FilePath -Version "3.0.0" -Metadata @{ Description = "Specific version 3 for checkout test" }
+        git commit -m 'Committing version 3'
+        Set-VVVersion -Path $FilePath -Version '3.0.0' -Metadata @{ Description = 'Specific version 3 for checkout test' }
         
         # Verify the file content matches the specific version content before checkout
         Get-Content -Path $FilePath | Should -Be $Version3Content
 
         # Checkout the version using the -Checkout parameter
-        Get-VVVersion -Path $FilePath -Checkout -Version "2.0.0"
+        Get-VVVersion -Path $FilePath -Checkout -Version '2.0.0'
 
         # Verify the file content matches the specific version content after checkout
         Get-Content -Path $FilePath | Should -Be $Version2Content
@@ -112,8 +185,8 @@ Describe "Integration Tests" {
         git reset HEAD --hard
         $RemoteTagsCountBefore = (git ls-remote --tags origin 'refs/tags/@VV*' | Where-Object { -not $_.EndsWith('^{}') }).Count
 
-        Set-VVVersion -Path $FilePath -Version "4.0.0" -Metadata $MetadataHash -WarningAction SilentlyContinue
-        { Push-VVVersion -Path $FilePath -Version "4.0.0" } | Should -Not -Throw
+        Set-VVVersion -Path $FilePath -Version '4.0.0' -Metadata $MetadataHash -WarningAction SilentlyContinue
+        { Push-VVVersion -Path $FilePath -Version '4.0.0' } | Should -Not -Throw
         
         $RemoteTagsCountAfter = (git ls-remote --tags origin 'refs/tags/@VV*' | Where-Object { -not $_.EndsWith('^{}') }).Count
 
@@ -127,11 +200,34 @@ Describe "Integration Tests" {
         git reset HEAD --hard
         $RemoteTagsCountBefore = (git ls-remote --tags origin 'refs/tags/@VV*' | Where-Object { -not $_.EndsWith('^{}') }).Count
 
-        Set-VVVersion -Path $FilePath -Version "5.0.0" -Metadata $MetadataHash -WarningAction SilentlyContinue
-        { Push-VVVersion -Tag "@VV/$FileName/v5.0.0" -Remote "origin" } | Should -Not -Throw
+        Set-VVVersion -Path $FilePath -Version '5.0.0' -Metadata $MetadataHash -WarningAction SilentlyContinue
+        { Push-VVVersion -Tag "@VV/$FileName/v5.0.0" -Remote 'origin' } | Should -Not -Throw
         
         $RemoteTagsCountAfter = (git ls-remote --tags origin 'refs/tags/@VV*' | Where-Object { -not $_.EndsWith('^{}') }).Count
 
         $RemoteTagsCountAfter | Should -BeExactly ($RemoteTagsCountBefore + 1)
+    }
+
+    It 'Can sync tag names for a renamed file using Sync-VVVersion' {
+        # Setup new file name
+        $NewFileName = 'renamed_testfile.txt'
+        $NewFilePath = Join-Path -Path $LocalRepoPath -ChildPath $NewFileName
+        
+        # Ensure current file name doesn't contain new name
+        $VersionsBeforeRename = Get-VVVersion -Path $FilePath
+        $VersionsBeforeRename.Path | Should -Not -Contain $NewFileName
+
+        Rename-Item -Path $FilePath -NewName $NewFileName
+
+        git add $NewFilePath
+        git commit -m 'added new version'
+
+        # Use Sync-VVVersion to update tags
+        { Sync-VVVersion -Path $NewFilePath -WarningAction SilentlyContinue } | Should -Not -Throw
+
+        # Verify tags are updated
+        $VersionsAfterRename = Get-VVVersion -Path $NewFilePath
+        $VersionsAfterRename.Count | Should -Be $VersionsBeforeRename.Count
+        $VersionsAfterRename.Path | Should -Contain $NewFileName
     }
 }
